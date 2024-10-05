eb73811862314cf075520a26d1fd6cf940269a41
UGRINOVICH CODE <91239358+pixxbe@users.noreply.github.com>
2024-10-05 18:01:47 +1000

    Add files via upload

--- labs/Assembly_1_lab.cpp
+++ labs/Assembly_1_lab.cpp
@@ -1,1 +1,37 @@
-1
+﻿// Assembly_1_lab.cpp: 7 Вариант
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+
+{
+	 char  y;
+	_asm
+	{
+		mov ax, 22; в ax кладем 22
+		mov bl, 3; в bl кладем 3
+		div bl; делим
+		mov ah, 0; зануляеми остаток
+		mov dl, al; перекладываем в dl, al
+
+		mov ax, 14; в ax кладем 14
+		mov bl, 5; в bl кладем 5
+		div bl; делим
+		mov ah, 0; зануляеми остаток
+
+		xchg al, dl; менеям значения al с dl
+		sub al, dl; вычитаем из al dl
+		add al, 3; в al кладем 22
+
+		mov bl, 5; в bl кладем 5
+		add bl, 5; в bl кладем 6
+		div bl; делим
+		sub al, 12; вычитаем из al 12
+
+		mov y, al; в y кладем  al
+
+	}
+	printf("%d", y);
+}
